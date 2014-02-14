17576c6ab67a9447cbe329be6110cc47e31aad3a
ecc311 <greewolf_3@hotmail.com>
2014-02-14 13:28:39 +0100

    Pujats tots els arxius 2

---
+++ prova1/src/prova/GestorAlumnes.java
@@ -0,0 +1,69 @@
+package prova;
+
+import prova.Persona;
+
+/**
+ * Clase que gestiona els atributs de l'alumne
+ * 
+ * @author eric
+ * 
+ */
+
+public class GestorAlumnes  {
+
+	private GestorAlumnesMati alumne2=new GestorAlumnesMati();
+	private GestorAlumnesTarda alumne1=new GestorAlumnesTarda();
+	private int tipus;
+	private static int id=4;
+	
+	/**
+	 * @return the alumne
+	 */
+	public Persona getAlumne()
+	{
+
+		if (this.tipus==2)
+		{
+			return this.alumne1.getAlumne();
+		}else
+		{
+			return this.alumne2.getAlumne();
+		}
+
+	}
+	/**
+	 * @param codi
+	 *            the codi to set
+	 *@param nom
+	 *            the nom to set
+	 *@param edat
+	 *            the edat to set
+	 * 
+	 */
+	public void setAlumne(String nom,int edat,int tipus,String password)
+	{
+		this.tipus=tipus; 
+		setId(id+1);
+
+		switch (tipus) {
+		   case 1:
+			   		this.alumne2.setAlumne(getId(), nom, edat, tipus,password);
+			   		break;
+
+		   case 2:
+		   
+			   		this.alumne1.setAlumne(getId(), nom, edat, tipus,password);
+			   		break;
+		   
+		  }  
+		
+		 }
+	public static int getId() {
+		return id;
+	}
+	public static void setId(int id) {
+		GestorAlumnes.id = id;
+	}
+
+
+}
---
+++ prova1/src/prova/GestorProfessors.java
@@ -0,0 +1,44 @@
+package prova;
+
+import prova.Professor;
+
+/**
+ * Clase que gestiona els atributs del professor
+ * 
+ * @author eric
+ * 
+ */
+public class GestorProfessors{
+	
+	static int id=9000;
+
+	public Professor professor = new Professor();
+
+	/**
+	 * @return the professor
+	 */
+
+	public Professor getProfessor() {
+
+		return this.professor;
+	}
+
+	/**
+	 * @param codi
+	 *            the codi to set
+	 *@param nom
+	 *            the nom to set
+	 *@param edat
+	 *            the edat to set
+	 * 
+	 */
+	public void setProfessor(String nom, int edat,String password) {
+		
+		id=id+1;
+		this.professor.setIdPersona(id);
+		this.professor.setNomPersona(nom);
+		this.professor.setEdatPersona(edat);
+		this.professor.setPasswordPersona(password);
+
+	}
+}
