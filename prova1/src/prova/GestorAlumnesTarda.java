57264c968a9e777470dbd883a0844e2910d63349
ecc311 <greewolf_3@hotmail.com>
2014-02-14 13:28:39 +0100

    Pujats tots els arxius

---
+++ prova1/src/prova/GestorAlumnesTarda.java
@@ -0,0 +1,39 @@
+package prova;
+
+public class GestorAlumnesTarda {
+
+private AlumneTarda alumne=new AlumneTarda();
+
+	
+	public AlumneTarda getAlumne()
+	{
+
+
+			return this.alumne;
+
+
+	}
+	/**
+	 * @param codi
+	 *            the codi to set
+	 *@param nom
+	 *            the nom to set
+	 *@param edat
+	 *            the edat to set
+	 **@param grup
+	 *            the grup to set
+	 * 
+	 */
+	public void setAlumne(int codi,String nom,int edat,int tipus,String password)
+	{
+			   		
+			   		this.alumne.setIdPersona(codi);
+					this.alumne.setNomPersona(nom);
+					this.alumne.setEdatPersona(edat);
+					this.alumne.setHorariPersona();
+					this.alumne.setGrupPersona();
+		   
+		   
+		  }  
+		
+}
